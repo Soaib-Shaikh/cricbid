f97486970d9f4c588dcef6c5a1967057b110b387
Soaib Shaikh <samshaikh1192@gmail.com>
2026-05-21 12:00:29 +0530

    add logo

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -41,7 +41,7 @@ export default function AdminLayout({
                     <img
                         src={logo}
                         alt="CricBid"
-                        className="h-10 w-20 object-contain"
+                        className="h-20 w-20 object-contain"
                     />
                 </div>
 
